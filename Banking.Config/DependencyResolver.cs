c4ef1d1837b255a4db1e7720f01afe6220caefa0
yesuprakash <55362817+yesuprakash@users.noreply.github.com>
2019-09-16 02:15:48 +0530

    Add files via upload

---
+++ Banking.Config/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using Banking.Data;
+using Banking.Data.Repositories;
+using Banking.Service.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Banking.Config
+{
+    public class DependencyResolver
+    {
+        public static void RegisterDependency(IServiceCollection _ServiceCollection)
+        {
+            //Adding DI
+            _ServiceCollection.AddTransient<IUserManagementRespository, UserManagementRespository>();
+            _ServiceCollection.AddTransient<IUserManagementService, UserManagementService>();
+            _ServiceCollection.AddTransient<IAccountRepository, AccountRepository>();
+            _ServiceCollection.AddTransient<IAccountService, AccountService>();
+            _ServiceCollection.AddTransient<IPaymentRepository, PaymentRepository>();
+            _ServiceCollection.AddTransient<IPaymentService, PaymentService>();
+        }
+    }
+}
